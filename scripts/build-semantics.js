5d81df576942bb8eb73a0c8790efdc51e45df43c
Craig Harrison <craigharrison112@gmail.com>
2018-04-01 04:26:13 -0400

    Changed how Semantic class works

--- scripts/build-semantics.js
+++ scripts/build-semantics.js
@@ -9,16 +9,16 @@ const semantics = [];
 
 const feelLikeAFailure = new Semantic('I feel like a failure', 'thought');
 
-feelLikeAFailure.stems([
+feelLikeAFailure.stems = [
   'I keep failing',
   'I can not succeed',
   'I always fail',
-  ...combos(`I ${combos('feel like', 'think I am', 'must be')} a failure`)
-]);
+  ...combos(`I ${combos('feel like', 'think I am', 'must be')} a failure`),
+];
 
-feelLikeAFailure.meta({
-  pattern: 'mislabeling'
-});
+feelLikeAFailure.meta = {
+  pattern: 'mislabeling',
+};
 
 semantics.push(feelLikeAFailure);
 
@@ -26,16 +26,16 @@ semantics.push(feelLikeAFailure);
 
 const feelLikeALoser = new Semantic('I feel like a loser', 'thought');
 
-feelLikeALoser.stems([
+feelLikeALoser.stems = [
   'I keep losing',
   'I always lose',
   ...combos(`I ${combos('never', 'can not')} ${combos('win', 'succeed')}`),
-  ...combos(`I ${combos('feel like', 'think I am', 'must be')} a loser`)
-]);
+  ...combos(`I ${combos('feel like', 'think I am', 'must be')} a loser`),
+];
 
-feelLikeALoser.meta({
-  pattern: 'mislabeling'
-});
+feelLikeALoser.meta = {
+  pattern: 'mislabeling',
+};
 
 semantics.push(feelLikeALoser);
 
@@ -43,12 +43,10 @@ semantics.push(feelLikeALoser);
 
 const gotRejected = new Semantic('I got rejected', 'event');
 
-gotRejected.stems([
-  ...combos(
-    `I did not get ${combos('selected', 'approved', 'promoted', 'accepted')}`
-  ),
-  ...combos(`I got ${combos('rejected', 'denied', 'turned down')}`)
-]);
+gotRejected.stems = [
+  ...combos(`I did not get ${combos('selected', 'approved', 'promoted', 'accepted')}`),
+  ...combos(`I got ${combos('rejected', 'denied', 'turned down')}`),
+];
 
 semantics.push(gotRejected);
 
@@ -56,12 +54,12 @@ semantics.push(gotRejected);
 
 const gotIntoArgument = new Semantic(
   'I got into an arguement with someone',
-  'event'
+  'event',
 );
 
-gotIntoArgument.stems([
-  ...combos(`I got into ${combos('an argument', 'a debate', 'a fight')}`)
-]);
+gotIntoArgument.stems = [
+  ...combos(`I got into ${combos('an argument', 'a debate', 'a fight')}`),
+];
 
 semantics.push(gotIntoArgument);
 
@@ -69,18 +67,16 @@ semantics.push(gotIntoArgument);
 
 const triedAndDidNotSucceed = new Semantic(
   'I tried but did not succeed',
-  'event'
+  'event',
 );
 
-triedAndDidNotSucceed.stems([
-  ...combos(
-    `${combos('I tried but', 'Even though I tried I')} ${combos(
-      'did not succeed',
-      'failed',
-      'did not make it'
-    )}`
-  )
-]);
+triedAndDidNotSucceed.stems = [
+  ...combos(`${combos('I tried but', 'Even though I tried I')} ${combos(
+    'did not succeed',
+    'failed',
+    'did not make it',
+  )}`),
+];
 
 semantics.push(triedAndDidNotSucceed);
 
@@ -88,28 +84,24 @@ semantics.push(triedAndDidNotSucceed);
 
 const havingRelationshipIssues = new Semantic(
   "I'm having relationship issues",
-  'situation'
+  'situation',
 );
 
-havingRelationshipIssues.stems([
-  ...combos(
-    `I am having ${combos('issues', 'problems', 'struggles')} in my ${combos(
-      'marriage',
-      'relationship'
-    )}`
-  ),
-  ...combos(
-    `My ${combos(
-      'spouse',
-      'partner',
-      'husband',
-      'wife',
-      'girlfriend',
-      'boyfriend',
-      'significant other'
-    )} and I are having ${combos('issues', 'problems', 'struggles')}`
-  )
-]);
+havingRelationshipIssues.stems = [
+  ...combos(`I am having ${combos('issues', 'problems', 'struggles')} in my ${combos(
+    'marriage',
+    'relationship',
+  )}`),
+  ...combos(`My ${combos(
+    'spouse',
+    'partner',
+    'husband',
+    'wife',
+    'girlfriend',
+    'boyfriend',
+    'significant other',
+  )} and I are having ${combos('issues', 'problems', 'struggles')}`),
+];
 
 semantics.push(havingRelationshipIssues);
 
@@ -117,26 +109,34 @@ semantics.push(havingRelationshipIssues);
 
 const havingFinancialIssues = new Semantic(
   "I'm having financial issues",
-  'situation'
+  'situation',
 );
 
-havingFinancialIssues.stems([
+havingFinancialIssues.stems = [
   'My finances are struggling',
   'I am having a hard time with money',
   'My savings are dwindling',
   'Have to take out a loan',
   'Not gonna have enough for rent',
   'Rent is due and do not have the money',
-  'Budget is tight'
-]);
+  'Budget is tight',
+];
 
 semantics.push(havingFinancialIssues);
 
 // =============================================================================
+const printRoots = true;
 
 const outPath = path.resolve(__dirname, '..', 'data', 'semantics.json');
-const outText = JSON.stringify(semantics.map(s => s.toJSON()), null, 2);
+const outText = JSON.stringify(
+  semantics.map((s) => {
+    if (printRoots) console.log(s.root);
+    return s.toJSON();
+  }),
+  null,
+  2,
+);
 
-fs.writeFile(outPath, outText, 'utf8', err => {
+fs.writeFile(outPath, outText, 'utf8', (err) => {
   if (err) throw err;
 });
